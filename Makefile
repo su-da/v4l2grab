f50a42bf2928e9a9744ca015f065e65625742770
Wenfeng CAI <caiwenfeng@suda.edu.cn>
2015-11-22 16:06:36 +0800

    Add a selection to display video by SDL window or save images.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 MAINTARGET := v4l2grab
 SOURCE := v4l2grab.c decoder_mjpeg.c
-CFLAGS += -Wall
-LDFLAGS += -lv4l2
+CFLAGS += -Wall -D_REENTRANT
+LDFLAGS += -lv4l2 -lSDL2 -lSDL2_image
 OBJS := ${SOURCE:.c=.o}
 
 all: $(MAINTARGET)
--- v4l2grab.c
+++ v4l2grab.c
@@ -16,6 +16,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <limits.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/time.h>
@@ -27,6 +28,9 @@
 
 #include "decoder.h"
 #include "decoder_mjpeg.h"
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 #define CLEAR(x) memset(&(x), 0, sizeof(x))
 
@@ -55,6 +59,22 @@ static void xioctl(int fh, int request, void *arg)
     }
 }
 
+#define TICK_INTERVAL    50
+
+Uint32 TimeLeft(void)
+{
+    Uint32 next_tick = 0;
+    Uint32 cur_tick;
+
+    cur_tick = SDL_GetTicks();
+    if (next_tick <= cur_tick) {
+        next_tick = cur_tick + TICK_INTERVAL;
+        return 0;
+    } else {
+        return (next_tick - cur_tick);
+    }
+}
+
 static void usage(FILE *fp, int argc, char **argv)
 {
     fprintf(fp,
@@ -63,17 +83,19 @@ static void usage(FILE *fp, int argc, char **argv)
             "-d | --device name   Video device name [/dev/video0]\n"
             "-h | --help          Print this message\n"
             "-c | --count         Number of frames to grab [3]\n"
+            "-n | --dry           Don't save images but display them\n"
             "",
             argv[0]);
 }
 
-static const char short_options[] = "d:hc:";
+static const char short_options[] = "d:hc:n";
 
 static const struct option
 long_options[] = {
         { "device", required_argument, NULL, 'd' },
         { "help",   no_argument,       NULL, 'h' },
         { "count",  required_argument, NULL, 'c' },
+        { "dry",    no_argument,       NULL, 'n' },
         { 0, 0, 0, 0 }
 };
 
@@ -95,6 +117,14 @@ int main(int argc, char **argv)
     unsigned char *out_buf = NULL;
     Decoder *decoder = NULL;
     int jpeg_size;
+    int dry = 0;
+
+    SDL_Window *sdlWindow;
+    SDL_Renderer *sdlRenderer;
+    SDL_RWops* buffer_stream;
+    SDL_Surface* picture;
+    SDL_Texture *texture;
+    SDL_Event event;
 
     for (;;) {
         int idx;
@@ -123,6 +153,10 @@ int main(int argc, char **argv)
             frame_count = strtol(optarg, NULL, 0);
             if (errno)
                 errno_exit(optarg);
+            break;
+
+        case 'n':
+            dry = 1;
             break;
 
         default:
@@ -195,6 +229,21 @@ int main(int argc, char **argv)
 
     xioctl(fd, VIDIOC_STREAMON, &type);
     decoder = decoder_mjpeg_create();
+    if (dry) {
+        frame_count = INT_MAX;
+
+        // Initialise everything.
+        SDL_Init(SDL_INIT_VIDEO);
+        IMG_Init(IMG_INIT_JPG);
+
+        sdlWindow = SDL_CreateWindow("Video Show", SDL_WINDOWPOS_UNDEFINED,
+                                     SDL_WINDOWPOS_UNDEFINED,
+                                     fmt.fmt.pix.width,
+                                     fmt.fmt.pix.height,
+                                     0);
+        sdlRenderer = SDL_CreateRenderer(sdlWindow, -1, 0);
+    }
+
     for (i = 0; i < frame_count; i++) {
         do {
             FD_ZERO(&fds);
@@ -209,6 +258,12 @@ int main(int argc, char **argv)
         if (r == -1) {
             perror("select");
             decoder_destroy(decoder);
+            if (dry) {
+                SDL_DestroyRenderer(sdlRenderer);
+                SDL_DestroyWindow(sdlWindow);
+                IMG_Quit();
+                SDL_Quit();
+            }
             return errno;
         }
 
@@ -217,26 +272,66 @@ int main(int argc, char **argv)
         buf.memory = V4L2_MEMORY_MMAP;
         xioctl(fd, VIDIOC_DQBUF, &buf);
 
-        sprintf(out_name, "out%03d.jpg", i);
-        fout = fopen(out_name, "w");
-        if (!fout) {
-            perror("Cannot open image");
-            exit(EXIT_FAILURE);
-        }
         jpeg_size = 0;
         jpeg_size = decoder_decode(decoder, &out_buf,
-                                   buffers[buf.index].start, buf.bytesused);
-        if ( jpeg_size > 0 ) {
-            fwrite(out_buf, jpeg_size, 1, fout);
-            free(out_buf);
-        } else
-            fwrite(buffers[buf.index].start, buf.bytesused, 1, fout);
-        fclose(fout);
+                                   buffers[buf.index].start,
+                                   buf.bytesused);
+        if (dry) {
+            // Create a stream based on our buffer.
+            if ( jpeg_size > 0 )
+                buffer_stream = SDL_RWFromMem(out_buf, jpeg_size);
+            else
+                buffer_stream = SDL_RWFromMem(buffers[buf.index].start,
+                                              buf.bytesused);
+
+            // Create a surface using the data coming out of the above stream.
+            picture = IMG_Load_RW(buffer_stream, 1);
+            texture = SDL_CreateTextureFromSurface(sdlRenderer, picture);
+            SDL_FreeSurface(picture);
+            SDL_RenderCopy(sdlRenderer, texture, NULL, NULL);
+            SDL_DestroyTexture(texture);
+            SDL_RenderPresent(sdlRenderer);
+            SDL_Delay(TimeLeft());
+
+            // event poll
+            SDL_PollEvent(&event);
+            switch (event.type) {
+            case SDL_KEYDOWN:
+            case SDL_KEYUP:
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                    i = INT_MAX;
+                break;
+            case SDL_QUIT:
+                i = INT_MAX;
+                break;
+            default:
+                break;
+            }
+        } else {
+            sprintf(out_name, "out%03d.jpg", i);
+            fout = fopen(out_name, "w");
+            if (!fout) {
+                perror("Cannot open image");
+                exit(EXIT_FAILURE);
+            }
+            if ( jpeg_size > 0 ) {
+                fwrite(out_buf, jpeg_size, 1, fout);
+                free(out_buf);
+            } else
+                fwrite(buffers[buf.index].start, buf.bytesused, 1, fout);
+            fclose(fout);
+        }
 
         xioctl(fd, VIDIOC_QBUF, &buf);
     }
 
     decoder_destroy(decoder);
+    if (dry) {
+        SDL_DestroyRenderer(sdlRenderer);
+        SDL_DestroyWindow(sdlWindow);
+        IMG_Quit();
+        SDL_Quit();
+    }
     type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     xioctl(fd, VIDIOC_STREAMOFF, &type);
     for (i = 0; i < n_buffers; ++i)
